966774a000384157c8db558c8b5b0dafb841aad5
JerryHanel <Jerry.Hanel@lynnco-scs.com>
2019-03-08 18:04:02 -0600

    Change of the name

--- hd-soap-service.ts
+++ hd-soap-service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class SoapService {
+export class HdSoapService {
   public host = ''; 
   public port = 80;
   public pathToAsmx = ''; 
